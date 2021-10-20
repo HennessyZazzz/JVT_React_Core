f31414c6c2f2312987517ac4afcca23f4dd84973
HennessyZazzz <60851577+HennessyZazzz@users.noreply.github.com>
2021-10-20 16:13:03 +0300

    Add project files.

---
+++ JVT_React_Core/Data/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JVT_React_Core.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JVT_React_Core.Data
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+    }
+}
---
+++ JVT_React_Core/Data/UserDbContext.cs
@@ -0,0 +1,24 @@
+﻿using JVT_React_Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JVT_React_Core.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity => { entity.HasIndex(o => o.Email).IsUnique(); });
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
